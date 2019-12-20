a439abc49deeebf66f320354614f04a9d8b5c328
mark-oblack <moblack42@gmail.com>
2019-12-20 14:41:16 -0500

    new code for navbar; cleaned up CSS files

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,28 +2,40 @@
 
 //Responsive Navbar
 
-function navResponsive() {
-    var x = document.getElementById("topNav");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
+function responsiveNav() {
+	var x = document.getElementById("myTopnav");
+	var y =document.getElementById("myTopnavItems");
+	if (x.className === "topnav" && y.className === "topnav-items") {
+	  x.className += " responsive";
+	  y.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	  y.className = "topnav-items";
+	}
 }
+
+// function navResponsive() {
+//     var x = document.getElementById("topNav");
+//     if (x.className === "navbar") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "navbar";
+//     }
+// }
 
 //Submenu
 
-function activateDropdown() {
-	var width = document.documentElement.clientWidth;
-	if(width < 600) {
-		var x = document.getElementById("dropdownMenu");
-		if (x.className === "dropdown-content") {
-			x.className += " show";
-		} else {
-			x.className = "dropdown-content";
-		}
-	}
-}
+// function activateDropdown() {
+// 	var width = document.documentElement.clientWidth;
+// 	if(width < 600) {
+// 		var x = document.getElementById("dropdownMenu");
+// 		if (x.className === "dropdown-content") {
+// 			x.className += " show";
+// 		} else {
+// 			x.className = "dropdown-content";
+// 		}
+// 	}
+// }
 
 //Illustrations Gallery Modals
 // Get the modal
