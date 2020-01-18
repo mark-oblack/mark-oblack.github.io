b14056870c07ff9ea2bfb7e34a98ae9fe9415906
mark-oblack <moblack42@gmail.com>
2020-01-18 16:27:35 -0500

    added modals to project pages; updated copyright to 2020

--- js/myscripts.js
+++ js/myscripts.js
@@ -42,10 +42,10 @@ function responsiveNav() {
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.myImg');
+var img = $(".myImg");
 var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-$('.myImg').click(function(){
+$('.myImg').click(function() {
 	modal.style.display = "block";
 	var newSrc = this.src;
 	modalImg.attr('src', newSrc);
